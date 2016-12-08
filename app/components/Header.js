030c90c5e2100cdbe3220a4edcef5aa4509e455e
leian-ivey <leian.ivey@thecontrolgroup.com>
2016-12-08 10:27:33 -0800

    Dumb Header

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,60 +4,58 @@ import SearchInput from 'react-search-input';
 import {hashHistory} from 'react-router';
 import Svg from 'components/svg/Svg';
 
-class Header extends React.Component {
-	render() {
-		const limitLength = (str, length) => `${str.substring(0, length)}...`;
+const Header = (props) => {
+	const limitLength = (str, length) => `${str.substring(0, length)}...`;
 
-		let {
-			title,
-			buttonHandler,
-			backButton,
-			archiveStatus,
-			archiveStatusToggle,
-			searchFilter,
-			searchTerm,
-			toggleSearch,
-			isSearching
-		} = this.props;
+	let {
+		title,
+		buttonHandler,
+		backButton,
+		archiveStatus,
+		archiveStatusToggle,
+		searchFilter,
+		searchTerm,
+		toggleSearch,
+		isSearching
+	} = this.props;
 
-		title = title ? <h1>{title.length > 20 ? limitLength(title, 20) : title}</h1>
-			: <Svg className="logo" svg="tfLogo" />;
+	title = title ? <h1>{title.length > 20 ? limitLength(title, 20) : title}</h1>
+		: <Svg className="logo" svg="tfLogo" />;
 
-		return (
-			<header>
-				{ backButton ?
-					<span className="header-btn" onClick={buttonHandler ? buttonHandler : () => hashHistory.goBack()} />
-				: null }
+	return (
+		<header>
+			{ backButton ?
+				<span className="header-btn" onClick={buttonHandler ? buttonHandler : () => hashHistory.goBack()} />
+			: null }
 
-				{ _.isUndefined(searchFilter) ? null
-					: <div id="searchFilter">
-						<span className="header-search" onClick={() => toggleSearch()}>
-							<Svg svg="search" className="header-search-icon" />
-						</span>
+			{ _.isUndefined(searchFilter) ? null
+				: <div id="searchFilter">
+					<span className="header-search" onClick={() => toggleSearch()}>
+						<Svg svg="search" className="header-search-icon" />
+					</span>
 
-						<SearchInput
-							className={`search-input ${isSearching ? '' : 'hidden'}`}
-							fuzzy={true}
-							value={searchTerm}
-							onChange={searchFilter}
-						/>
-					</div>
-				}
+					<SearchInput
+						className={`search-input ${isSearching ? '' : 'hidden'}`}
+						fuzzy={true}
+						value={searchTerm}
+						onChange={searchFilter}
+					/>
+				</div>
+			}
 
-				{title}
+			{title}
 
-				{ _.isUndefined(searchFilter) || !isSearching ? null
-					: <span className="header-done" onClick={() => toggleSearch()}>Done</span>
-				}
+			{ _.isUndefined(searchFilter) || !isSearching ? null
+				: <span className="header-done" onClick={() => toggleSearch()}>Done</span>
+			}
 
-				{ _.isUndefined(archiveStatus) && _.isUndefined(archiveStatusToggle) || isSearching ? null :
-					archiveStatus ? <span className="header-done" onClick={() => archiveStatusToggle()}>Done</span>
-					: <span className="header-edit" onClick={() => archiveStatusToggle()}>Edit</span>
-				}
-			</header>
-		);
-	}
-}
+			{ _.isUndefined(archiveStatus) && _.isUndefined(archiveStatusToggle) || isSearching ? null :
+				archiveStatus ? <span className="header-done" onClick={() => archiveStatusToggle()}>Done</span>
+				: <span className="header-edit" onClick={() => archiveStatusToggle()}>Edit</span>
+			}
+		</header>
+	);
+};
 
 Header.propTypes = {
 	title: React.PropTypes.string,
