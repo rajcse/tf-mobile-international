88ed209f2a3bbd825bcca3e93ee5596c3f76dfa4
Mehmet Koc <mehmetkoc182@gmail.com>
2017-03-29 14:56:59 -0700

    so it points to its own new dev site.json on dev

--- config/development.js
+++ config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-	API_ROOT: 'http://www.api.truthfinder.app.public-records.local.tcg.io',
+	API_ROOT: 'http://www.api.international.app.public-records.local.tcg.io',
 	MAPBOX_TOKEN: 'pk.eyJ1IjoidGhlY29udHJvbGdyb3VwIiwiYSI6ImNpZW9zcHRxdDBoendzM2ttNm1tdDAzY3AifQ.ZibEsq1WAjWEztnvnJBH_g'
 };
 
