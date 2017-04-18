8adfc338a8cff4b61cb39208fc82c992b3e2fea6
Mehmet Koc <mehmetkoc182@gmail.com>
2017-04-18 11:52:17 -0700

    new api

--- config/production.js
+++ config/production.js
@@ -1,4 +1,4 @@
 module.exports = {
-	API_ROOT: 'https://api2.truthfinder.com',
+	API_ROOT: 'https://intl.truthfinder.com',
 	MAPBOX_TOKEN: 'pk.eyJ1IjoidGhlY29udHJvbGdyb3VwIiwiYSI6ImNpZW9zcHRxdDBoendzM2ttNm1tdDAzY3AifQ.ZibEsq1WAjWEztnvnJBH_g'
 };
